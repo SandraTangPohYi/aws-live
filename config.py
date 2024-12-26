9b0eac7977343b65712a731ed283221cc4d3c27c
SandraTangPohYi <st9491454@gmail.com>
2024-12-26 13:16:52 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cvik86k8ya7v.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "sandratang-employee"
+customregion = "us-east-1"
+
